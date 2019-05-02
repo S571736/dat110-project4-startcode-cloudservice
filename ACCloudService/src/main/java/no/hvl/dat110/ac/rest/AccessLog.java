66c8ce904d462f1ca6f8247634e35cc484073b63
S571736 <Sondre.Gjesdal@gmail.com>
2019-05-02 11:15:45 +0200

    får eg pusha denne tho?

--- ACCloudService/src/main/java/no/hvl/dat110/ac/rest/AccessLog.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/rest/AccessLog.java
@@ -9,36 +9,41 @@ public class AccessLog {
 	
 	private AtomicInteger cid;
 	protected ConcurrentHashMap<Integer, AccessEntry> log;
-	
+	private Gson gson;
+
 	public AccessLog () {
 		this.log = new ConcurrentHashMap<Integer,AccessEntry>();
 		cid = new AtomicInteger(0);
+		gson = new Gson();
 	}
 
 	// TODO: add an access entry for the message and return assigned id
 	public int add(String message) {
-		
-		int id = 0;
-		
+
+		int id = cid.incrementAndGet();
+		log.put(id, gson.fromJson(message, AccessEntry.class));
 		return id;
 	}
 		
 	// TODO: retrieve a specific access entry 
 	public AccessEntry get(int id) {
-		
-		return null;
+
+
+		return log.get(id);
 		
 	}
 	
 	// TODO: clear the access entry log
 	public void clear() {
+		log.clear();
+		cid.set(0);
 		
 	}
 	
 	// TODO: JSON representation of the access log
 	public String toJson () {
     	
-		String json = null;
+		String json = gson.toJson(log);
     	
     	return json;
     }
